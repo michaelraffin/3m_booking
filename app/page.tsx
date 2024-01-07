4c6ad4df73356c2a704cb048b1232f2b9abcd226
Michael <michaelraffinpaculba@gmail.com>
2024-01-07 12:10:20 +0800

    Fix ClassNae

--- app/page.tsx
+++ app/page.tsx
@@ -33,9 +33,6 @@ export default function Home() {
 
   const scrollRef = useRef<HTMLDivElement>(null);
   const cartRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-
-  }, [products])
 
   const clearDate = () => {
     setSelectedService({ title: null, id: null, price: 0 })
@@ -89,7 +86,7 @@ export default function Home() {
     try {
       if (date != undefined) {
         return (<>
-          <div className={classDetails}>
+          <div className={`${classDetails}`}>
             <div><Badge variant="outline">{moment(date.from).format('LL')}</Badge></div>
             <div><Badge variant="outline">{moment(date.to).format('LL')}</Badge></div>
           </div>
@@ -493,25 +490,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          {/* <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a> */}
-        </div>
+       </div>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
