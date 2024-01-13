8708402300cd092117b9670fc25cec16999aca68
Michael <michaelraffinpaculba@gmail.com>
2024-01-13 15:50:40 +0800

    Add URL

--- Utils/login_service.js
+++ Utils/login_service.js
@@ -13,7 +13,8 @@ const getURL = () => {
     let url =
       process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
       process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://192.168.1.148:3001/authLogin'
+      'https://booking.3mevents.com/authLogin'
+      // 'http://192.168.1.148:3001/authLogin'
     // Make sure to include `https://` when not localhost.
     url = url.includes('http') ? url : `https://${url}`
     // Make sure to include a trailing `/`.
