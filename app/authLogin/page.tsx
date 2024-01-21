dab9bd31640e21bb7ebb1af3c80d645f1c5bfb34
Michael <michaelraffinpaculba@gmail.com>
2024-01-21 14:51:32 +0800

    replace

--- app/authLogin/page.tsx
+++ app/authLogin/page.tsx
@@ -26,12 +26,12 @@ export default function Auth() {
   useEffect(() => {
     try {
       localStorage.clear()
-      router.push('/dashboard');
+ 
       const accessToken = `${currentUrl}`
       const access_token = new URLSearchParams(accessToken.split('#')[1]).get('access_token');
       const provider_token = new URLSearchParams(accessToken.split('#')[1]).get('provider_token');
       const refresh_token = new URLSearchParams(accessToken.split('#')[1]).get('refresh_token');
-      
+      router.replace('/dashboard');
         // localStorage.setItem('access_token', `${access_token}`);
       // localStorage.setItem('provider_token', `${provider_token}`);
       // localStorage.setItem('refresh_token', `${refresh_token}`);
